1856de160e1929a6ab194948f153cfcb4a40d764
muiddd <muidabdulmuid99@gmail.com>
2024-10-31 21:40:18 +0700

    final commit percobaan 4

--- SearchNilai01.java
+++ SearchNilai01.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
 public class SearchNilai01 {
     public static void main(String[] args) {
-        int [] arrNilai = {80, 85, 78, 96, 90, 82, 86};
-        int key = 90;
+        Scanner sc= new Scanner(System.in);
+
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int jumlahNilai = sc.nextInt();
+
+        int [] arrNilai = new int [jumlahNilai];            ;
+
+        for (int i = 0; i< arrNilai.length; i++){
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i+1) + " : ");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        int key = sc.nextInt();
         int hasil = 0;
 
         for (int i = 0; i< arrNilai.length; i++){
@@ -11,7 +24,11 @@ public class SearchNilai01 {
             }
         }
         System.out.println();
-        System.out.println("Nilai "+key+" ketemu di indeks ke-"+hasil);
+        if (hasil!=0){
+            System.out.println("Nilai "+key+" ketemu, merupakan nilai mahasiswa ke-: "+(hasil+1));
+        } else {
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        }
         System.out.println();
     }
 }
