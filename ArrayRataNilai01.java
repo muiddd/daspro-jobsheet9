c7e52312ddae38831e73e9d1cc00eb23c2594b65
muiddd <muidabdulmuid99@gmail.com>
2024-10-31 23:02:34 +0700

    final commit percobaan 3

--- ArrayRataNilai01.java
+++ ArrayRataNilai01.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class ArrayRataNilai {
+public class ArrayRataNilai01 {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
 
